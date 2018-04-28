b41053cc7bd753c9cc480b54931cbb2d52954fd0
Charli Santiago <badcoderspaik@gmail.com>
2018-04-28 16:56:31 +0600

    добавлена мобильная версия формы отправки контента

--- admin/js/main.js
+++ admin/js/main.js
@@ -17,7 +17,8 @@ $(function () {
       top: "10px"
     }),
 
-    add_content_form = $("form[name='add_content_form']"),//jquery объект формы добавления контента из файла add_content_form.html
+    add_content_form = $("#add_content_form"),//jquery объект формы добавления контента из файла add_content_form.html
+    mob_add_content_form = $("#mob_add_content_form"),
     //Объект загрузчика контента на сервер
     form_data_loader = new APP.FormDataLoader({
       url: "../admin/modules/add_content_form.php",//url, куда следует отправить данные формы
@@ -33,7 +34,77 @@ $(function () {
 
   form_notification.append();//добавление уведомления в DOM
 
-  add_content_form.validate(//подключение к форме плагина валидации
+  add_content_form.validate(//подключение к форме (десктоп версия) плагина валидации
+    {
+      book_title: {
+        required: true,
+
+        messages: {
+          empty: "Название не может быть пустым!",
+          minLength: "Название должно содержать более 3х символов!",
+          maxLength: "Название не должно превышать 100 символов",
+        },
+
+        minLength: 3,
+        maxLength: 100,
+
+      },
+
+      autor: {
+        required: true,
+        minLength: 3,
+
+        messages: {
+          empty: "Имя автора не может быть пустым!",
+          minLength: "Имя автора должно содержать более 3х символов!"
+        }
+
+      },
+
+      year: {
+        required: true,
+
+        messages: {
+          empty: "Год не может быть пустым!",
+          expression: "Год должен быть указан цифрами"
+        },
+
+        expression: new RegExp("^\\d+$")
+
+      },
+
+      description: {
+        required: true,
+        minLength: 3,
+
+        messages: {
+          empty: "Описание не может быть пустым!",
+          minLength: "Описание должно содержать более 3х символов!"
+        }
+
+      },
+
+      cover: {
+        required: true,
+
+        messages: {
+          empty: "Не выбрана обложка!"
+        }
+
+      },
+
+      book_file: {
+        required: true,
+
+        messages: {
+          empty: "Не выбран файл!"
+        }
+      }
+
+    }, form_data_loader.query
+  );
+
+  mob_add_content_form.validate(//подключение к форме ( mobile верия ) плагина валидации
     {
       book_title: {
         required: true,
